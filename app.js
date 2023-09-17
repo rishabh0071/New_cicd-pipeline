ebf5be4007a4c83eaab2902a322b2b03d90a2e69
rishabh0071 <57853470+rishabh0071@users.noreply.github.com>
2023-09-18 00:56:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
     });
 
-    app.listen(8080, () => {
+    app.listen(8000, () => {
         console.log('App listening on port 8000');
         });
